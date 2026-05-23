8c5438473d508283083a930b9e98ef6388043671
Tung Nguyen <nstung@gmail.com>
2026-05-23 11:23:44 +0700

    chore(docs-rn): add .gitignore, untrack .next build cache

--- apps/docs-rn/.next/static/chunks/apps_docs-rn_src_app_(main)_components_[slug]_page_tsx_1dd3e2ab._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_df541132._.js",
-  "static/chunks/apps_docs-rn_src_components_docs_c714bb5a._.js"
-],
-    source: "dynamic"
-});
--- apps/docs-rn/.next/static/chunks/apps_docs-rn_src_app_(main)_layout_tsx_88ea9efd._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_560a4476._.js",
-  "static/chunks/apps_docs-rn_src_a72ec1ce._.js"
-],
-    source: "dynamic"
-});
